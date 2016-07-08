4c303e4e2e0a50517177c69b442f178d86fb9842
Ashley Hindle <ashley@smellynose.com>
2016-07-08 22:14:17 +0100

    Update provisioner.sh

--- provisioner.sh
+++ provisioner.sh
@@ -1,5 +1,8 @@
 set -x
 
+curl -L https://github.com/docker/compose/releases/download/1.8.0-rc2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
 cd /tmp/
 git clone https://github.com/mattermost/mattermost-docker.git
 cd mattermost-docker
